862e96277f9a23e520a68ed495509f709c58ee33
Alexandre CONTASSOT <alexandrecontassot@hotmail.fr>
2021-05-15 12:41:17 +0200

    fix launch

--- sanctuary-prettier.sh
+++ sanctuary-prettier.sh
@@ -1,3 +1,5 @@
 set -e
-node_modules/.bin/prettier --write "$1"
-node_modules/sanctuary-prettier.js "$1"
+
+./node_modules/.bin/prettier --write "$1"
+
+./node_modules/sanctuary-prettier/sanctuary-prettier.js "$1"
