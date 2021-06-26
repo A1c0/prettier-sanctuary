e8c09c8c72094e9935e3a8a9521c0061a4b95916
Alexandre CONTASSOT <alexandrecontassot@hotmail.fr>
2021-06-26 14:44:24 +0200

    wrap when declaration is too long

--- app/wrap-curry.js
+++ app/wrap-curry.js
@@ -1,17 +1,17 @@
 const {mapOnLines, splitOnEoLNotIgnored} = require("./ignore-line");
-const {when, concat, pipe, flip, replaceAll, MAX_LENGTH} = require("./utils");
+const {when, concat, pipe, flip, replaceAll, MAX_LENGTH, INDENT} = require("./utils");
 const {space} = require("./common");
 
 const SUBSTITUTE_BEGIN_PARENTHESIS = '█';
 const SUBSTITUTE_END_PARENTHESIS = '▓';
 
 const splitOnParenthesis = pipe([
-    JSON.stringify,
-    replaceAll("(")('", ["(", "'),
-    replaceAll(")")('", ")"], "'),
-    flip(concat)("["),
-    concat("]"),
-    JSON.parse,
+  JSON.stringify,
+  replaceAll("(")('", ["(", "'),
+  replaceAll(")")('", ")"], "'),
+  flip(concat)("["),
+  concat("]"),
+  JSON.parse,
 ]);
 
 const initArray = a => Array.isArray(a) ? a : Array.of(a);
@@ -31,8 +31,6 @@ const replaceFirstAndLastParenthesisBySubstitute = l => {
 const wrapLine = (line) => {
   const workingLine = /^ *\(.*?$/.exec(line) ? replaceFirstAndLastParenthesisBySubstitute(line) : line;
   const array = splitOnParenthesis(workingLine);
-  console.log(array);
-  console.log(Array.isArray(array));
   let [head, ...tail] = array.map(concatDeepString).filter(x => (Boolean(x.trim())));
   const newTail = tail.slice(1).map(x => `${space(head.length)}${x}`)
   head = `${head}${tail[0] || ''}`;
@@ -43,8 +41,26 @@ const wrapLine = (line) => {
     x);
 }
 
+const isDeclarationTooLong = arr => {
+  const isDeclaration = arr[0].includes(" = ");
+  const eachLineIsTooLong = arr.map(x => x.length >= MAX_LENGTH).reduce((a, b) => a || b);
+  return isDeclaration && eachLineIsTooLong;
+}
+
+const wrapDeclaration = arr => {
+  const [currentHead, ...currentTail] = arr;
+  const newHead = currentHead.replace(/^(.* = ).*?$/, "$1");
+  const headRest = currentHead.replace(/^.* = (.*?)$/, "$1").replace (/^/,space(INDENT));
+  const newTail = [headRest, ...currentTail.map(l => l.slice(newHead.length - INDENT))];
+  return [newHead, ...newTail];
+}
+
 const wrapLineDeep = x => {
-  let text = wrapLine(x).join('\n');
+  let text = wrapLine(x);
+  if (isDeclarationTooLong(text)) {
+    text = wrapDeclaration(text);
+  }
+  text = text.join('\n');
   text = text.replaceAll(SUBSTITUTE_END_PARENTHESIS, ')')
     .replaceAll(SUBSTITUTE_BEGIN_PARENTHESIS, '(')
     .replaceAll(/\n *([\);,])/g, "$1");
