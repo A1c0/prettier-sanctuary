c62ce606bda8052878d236b38f76be77375b6e4f
Wai Lin Kyaw <wailink13@gmail.com>
2021-09-21 12:02:45 +0630

    add directory support when format

--- prettier-sanctuary.js
+++ prettier-sanctuary.js
@@ -1,6 +1,7 @@
 #!/usr/bin/env node
 "use strict";
 const fs = require("fs");
+const path = require('path');
 const {pipe, tap, split, join, map} = require("./app/utils");
 const {addIgnoreLines} = require("./app/ignore-line");
 const {defFormat} = require("./app/def-format");
@@ -10,6 +11,7 @@ const {wrapCurry} = require('./app/wrap-curry');
 
 const readTextFile = fileName => fs.readFileSync(fileName).toString();
 const writeTextFile = fileName => text => fs.writeFileSync(fileName, text);
+const isDirectory = path => fs.statSync(path).isDirectory();
 
 // customReformat :: Array {line: String, ignored: Boolean} -> Array {line: String, ignored: Boolean}
 const customReformat = pipe([
@@ -29,4 +31,21 @@ const bash = filePath => pipe([
   tap(writeTextFile(filePath))
 ])(filePath);
 
-bash(process.argv[2])
+function formatFilesInTheWholeDirectory ( dir ) {
+  fs.readdir(dir, (err, list) => {
+    list.forEach( file => {
+      const filePath = path.join(dir, file);
+      fs.stat(filePath, (err, stat) => {
+        if (stat.isDirectory()) {
+            formatFilesInTheWholeDirectory(filePath);
+        } else {
+            bash(filePath);
+        }
+      })
+    });
+  });
+}
+
+const f = process.argv[2];
+
+isDirectory(f) ? formatFilesInTheWholeDirectory(f) : bash(f);
