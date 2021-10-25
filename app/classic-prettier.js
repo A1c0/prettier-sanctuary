3c1fbbe0be397c3706db4db8ea7c1abcd6baa798
Alexandre CONTASSOT <alexandrecontassot@hotmail.fr>
2021-10-25 17:17:07 +0200

    Fixes

--- app/classic-prettier.js
+++ app/classic-prettier.js
@@ -1,24 +1,9 @@
-const {listFiles} = require("../lib/list-files");
-const {exec} = require("child_process");
+const prettier = require("prettier");
+const fs = require("fs");
 
-const isYarnPackageManager = listFiles(process.cwd())
-  .map(f => ["yarn.lock", ".yarnrc.yml"].includes(f))
-  .reduce((a, b) => a || b, false);
+const applyPrettier = (filePath, options) =>  {
+  const text = fs.readFileSync(filePath, 'utf8');
+  return prettier.format(text, Object.assign({parser:"babel"}, options));
+};
 
-const prettier = isYarnPackageManager ? "yarn prettier" : "prettier";
-
-const execPromise = (command) =>
-  new Promise((resolve, reject) => {
-    exec(command, (error, stdout, stderr) => {
-      if (error) {
-        reject(`exec error: ${error}`);
-      } else if (stderr) {
-        reject(stderr);
-      }
-      resolve(stdout);
-    });
-  });
-
-const applyPrettierOnFile = file => execPromise(`${prettier} ${file}`)
-
-module.exports = {applyPrettierOnFile}
+module.exports = {applyPrettier};
--- prettier-sanctuary.js
+++ prettier-sanctuary.js
@@ -1,15 +1,16 @@
 #!/usr/bin/env node
 "use strict";
 const fs = require("fs");
+const {performance} = require('perf_hooks');
 const {pipe, tap, split, join, map} = require("./app/utils");
 const {addIgnoreLines} = require("./app/ignore-line");
 const {defFormat} = require("./app/def-format");
 const {forceExtendPipe} = require("./app/force-extend-pipe");
 const {parenthesisCurry} = require("./app/parenthesis-curry");
 const {wrapCurry} = require('./app/wrap-curry');
-const {listFiles} = require("./lib/list-files");
+const {listFilesByArgs} = require("./lib/list-files");
 const COLOR = require("./lib/color");
-const {applyPrettierOnFile} = require("./app/classic-prettier");
+const {applyPrettier} = require("./app/classic-prettier");
 
 const readTextFile = fileName => fs.readFileSync(fileName).toString();
 const writeTextFile = fileName => text => fs.writeFileSync(fileName, text);
@@ -35,18 +36,22 @@ const applySanctuaryFormattingOnFile = filePath => pipe([
 
 const formatFile = async file =>  {
   const t0 = performance.now();
-  await applyPrettierOnFile(file)
+  await applyPrettier(file)
   applySanctuaryFormattingOnFile(file);
   const t1 = performance.now();
-  console.log(COLOR.dim + file.replace(process.cwd(), '') + COLOR.reset + ` ${t1-t0}ms`);
+  console.log(COLOR.dim + file.replace(`${process.cwd()}/`, '') + COLOR.reset + ` ${Math.round(t1-t0)}ms`);
 }
 
 const bash = async arg => {
-  const files = listFiles(arg);
+  const files = listFilesByArgs(arg);
+  if (files.length === 0) {
+    console.log("Usage: prettier-sanctuary [file/dir/glob ...]");
+    process.exit(1);
+  }
   performance.now();
   for (const file of files) {
     await formatFile(file)
   }
 }
 
-bash(process.argv[2])
+bash(process.argv.slice(2))
